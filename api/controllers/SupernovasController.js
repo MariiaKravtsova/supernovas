c6a67896da59d9841bc7c4c0b03382bdbc3682d0
Dillon Stone <dilnn760@hotmail.com>
2016-08-15 00:08:28 -0400

    added the methods and routes for create/update/delete novas
    created front end functionality for creating, updating, and deleting novas
    
    im going to sleep

--- api/controllers/SupernovasController.js
+++ api/controllers/SupernovasController.js
@@ -6,21 +6,42 @@
  */
 
 module.exports = {
- index: function index(req, res) {
+    index: function index(req, res) {
 //	 console.dir('hitting the controller');
-	 Supernovas.find().exec(function(errors, supernovas) {
-		 return res.view('table', {
-			 supernovas: supernovas
-		 });
-	 });
- },
- delete: function(req, res) {
-    Supernovas.destroy({'sn' : req.param('sn')}).exec(function(err, result){
-      return res.ok();
-    })
- },
-  update: function(req, res){
-
-  }
+        Supernovas.find().exec(function (errors, supernovas) {
+            return res.view('table', {
+                supernovas: supernovas
+            });
+        });
+    },
+    delete: function (req, res) {
+        Supernovas.destroy({'sn': req.param('sn')}).exec(function (err, result) {
+            if (err) {
+                return res.badRequest(err);
+            } else {
+                return res.ok();
+            }
+        })
+    },
+    update: function (req, res) {
+        var nova = req.params.all();
+        Supernovas.update({sn: nova.sn}, nova).exec(function (err, updated) {
+            if (err) {
+                return res.badRequest(err);
+            } else {
+                return res.ok();
+            }
+        })
+    },
+    create: function(req, res){
+        var nova = req.params.all();
+        Supernovas.create(nova).exec(function(err, newNova){
+            if (err) {
+                return res.badRequest(err);
+            } else {
+                return res.ok();
+            }
+        });
+    }
 
 };
--- config/routes.js
+++ config/routes.js
@@ -22,44 +22,54 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+     * etc. depending on your default view engine) your home page.              *
+     *                                                                          *
+     * (Alternatively, remove this and add an `index.html` file in your         *
+     * `assets` directory)                                                      *
+     *                                                                          *
+     ***************************************************************************/
 
-  'get /': {
-    view: 'homepage',
-    locals: {
-     layout: 'layout'
-      }
-  },
+    'get /': {
+        view: 'homepage',
+        locals: {
+            layout: 'layout'
+        }
+    },
 
-  'get /table': {
-    controller: 'Supernovas',
-    action: 'index',
-    locals: {
-       layout: 'table'
-      }
-   },
-   'delete supernova/delete': {
-     controller: 'Supernovas',
-     action: 'delete'
-   }
+    'get /table': {
+        controller: 'Supernovas',
+        action: 'index',
+        locals: {
+            layout: 'table'
+        }
+    },
+    'delete supernova/delete': {
+        controller: 'Supernovas',
+        action: 'delete'
+    },
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
- //TODO: add 'import' route, with a JS controller.
+    'put supernova/update': {
+        controller: 'Supernovas',
+        action: 'update'
+    },
+
+    'post supernova/create': {
+        controller: 'Supernovas',
+        action: 'create'
+    }
+
+    /***************************************************************************
+     *                                                                          *
+     * Custom routes here...                                                    *
+     *                                                                          *
+     * If a request to a URL doesn't match any of the custom routes above, it   *
+     * is matched against Sails route blueprints. See `config/blueprints.js`    *
+     * for configuration options and examples.                                  *
+     *                                                                          *
+     ***************************************************************************/
+    //TODO: add 'import' route, with a JS controller.
 
 };
